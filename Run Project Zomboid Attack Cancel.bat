92ad1b6165e9031dd5e2d6a527228e08008e4635
needitem <th07290828@gmail.com>
2026-03-31 20:07:36 +0900

    Make launcher bat find AutoHotkey

--- Run Project Zomboid Attack Cancel.bat
+++ Run Project Zomboid Attack Cancel.bat
@@ -1,4 +1,22 @@
 @echo off
 setlocal
 set "DIR=%~dp0"
-start "" "%DIR%AutoHotkey64.exe" "%DIR%project-zomboid-attack-cancel.ahk"
+set "AHK=%DIR%AutoHotkey64.exe"
+if exist "%AHK%" goto run
+
+set "AHK=%LocalAppData%\Programs\AutoHotkey\v2\AutoHotkey64.exe"
+if exist "%AHK%" goto run
+
+set "AHK=%ProgramFiles%\AutoHotkey\v2\AutoHotkey64.exe"
+if exist "%AHK%" goto run
+
+set "AHK=%ProgramFiles(x86)%\AutoHotkey\v2\AutoHotkey64.exe"
+if exist "%AHK%" goto run
+
+echo AutoHotkey64.exe was not found.
+echo Use the portable package, or install AutoHotkey v2.
+pause
+exit /b 1
+
+:run
+start "" "%AHK%" "%DIR%project-zomboid-attack-cancel.ahk"
